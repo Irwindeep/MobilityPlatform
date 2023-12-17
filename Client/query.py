dacd0ce6522a0ef2980ea9546a7fc1f24a8a33df
Irwin <120407944+Irwindeep@users.noreply.github.com>
2023-12-17 23:19:42 +0530

    Create and Paste query.py

---
+++ Client/query.py
@@ -0,0 +1,53 @@
+import paramiko
+from cryptography.fernet import Fernet
+from Client_Server import reg_name, reg_uid, cyc
+import base64
+
+
+def decrypt_data(key, data):
+    cipher_suite = Fernet(key)
+    decrypted_data = cipher_suite.decrypt(data).decode()
+    return decrypted_data
+
+class SMTH():
+    def smth(self):
+        x = input("Enter Payment Secret Code: ")
+        if x == "PAYMENT_DONE":
+            return True
+        else:
+            return False
+
+trial = 0
+while trial < 5:
+    y = SMTH().smth()
+    if y == True:
+        print("Payment Has been made successfully")
+        usr = "Host"
+        pas = "Raspberry"
+        ip = "192.168.187.197"
+        ssh = paramiko.SSHClient()
+        ssh.load_system_host_keys()
+        ssh.connect(ip, username=usr, password=pas)
+        print("Connected to Host Server")
+        #key = Fernet.generate_key()
+        #cipher = Fernet(key)
+        #name = cipher.encrypt(reg_name.encode())
+        #uid = cipher.encrypt(reg_uid.encode())
+        #selection = cipher.encrypt(cyc.encode())
+        
+        #key_str = base64.urlsafe_b64encode(key).decode()
+        stdin, stdout, stderr = ssh.exec_command(f"python3 ~/Project/Host.py {reg_name} {reg_uid} {cyc}")
+        for line in stdout:
+            print(line.strip())
+        for line in stderr:
+            print(line.strip())
+        ssh.close()
+        break
+    else:
+        print("Invalid Code, Try again ("+str(4-trial)+" trials left)")
+        if trial == 4:
+            print("Maximum Number of Attempts Reached")
+            break
+        trial += 1
+else:
+    exit(1)
